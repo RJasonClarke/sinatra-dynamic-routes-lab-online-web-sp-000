63eb23f55d733ec894aaa22038c49ab9d0331d95
Robert Clarke <r.jason.clarke@gmail.com>
2021-06-14 17:59:23 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -34,7 +34,7 @@ class App < Sinatra::Base
     @num2 = params[:number2].to_i
     case @oper
       when "subtract"
-        (@num1 - @num2).to_i
+        (@num1 - @num2).to_s
       when "add"
         (@num1 + @num2).to_s
       when "multiply"
